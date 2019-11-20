36772f55265a06dad9057542ab3bad013459e5a5
Mauricio Quiros <mqbravo@gmail.com>
2019-11-19 21:06:16 -0600

    Fix de chequeo de tipo en llamada futura de param
    Se encuentra un error de chequeo de tipos similar al que sucedia con la asignacion por medio de una llamada futura. Se utiliza el mismo metodo de resolucion

--- src/Triangle/ContextualAnalyzer/FutureCallExpression.java
+++ src/Triangle/ContextualAnalyzer/FutureCallExpression.java
@@ -7,6 +7,11 @@ public class FutureCallExpression {
     private TypeDenoter typeDenoterToCheck;
     private Expression E;
 
+    /**
+     * Constructor for a call expression with a future declaration
+     * @param typeDenoterToCheck The expected type for the call expression to have
+     * @param e The call expression itself
+     */
     public FutureCallExpression(TypeDenoter typeDenoterToCheck, Expression e) {
         this.typeDenoterToCheck = typeDenoterToCheck;
         E = e;
